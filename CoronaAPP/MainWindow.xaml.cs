5480fccfdca8b12baad4e18e439a14ac8071956f
mttti <38765349+mttti@users.noreply.github.com>
2022-06-07 19:27:07 +0200

    UpdateGUI

--- CoronaAPP/MainWindow.xaml.cs
+++ CoronaAPP/MainWindow.xaml.cs
@@ -42,37 +42,62 @@ namespace CoronaAPP
         }
         List<Root> CountriesList = new List<Root>();
 
-        public async void DownloadJasonData()
+        private void DownloadJasonData()
         {
-            HttpClient httpClient = new HttpClient();
-
-            string url = "https://covid-19.dataflowkit.com/v1";
-
-            var httpResponse = await httpClient.GetAsync(url);
-
-            string jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+            WebClient client = new WebClient();
+            client.Headers.Add("Accept", "application/json");
+            string jsonResponse = client.DownloadString("https://covid-19.dataflowkit.com/v1");
 
             CountriesList = JsonConvert.DeserializeObject<List<Root>>(jsonResponse);
+
+            CountriesList = CountriesList.OrderBy(x => x.Country_text).ToList();
+
         }
 
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();         
             DownloadJasonData();
             UpdateGui();
         }
 
         private void UpdateGui()
         {
-
             SelectCountry.Items.Clear();
-
+            SelectCountry.Items.Add("World");
             foreach (var item in CountriesList)
             {
+                if(item.Country_text != "World")
                 SelectCountry.Items.Add(item.Country_text);
             }
 
             SelectCountry.SelectedIndex = 0;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            string country = "";
+            string date = "";
+            string deaths = "";
+            string cases = "";
+
+
+            for (int i = 0; i < CountriesList.Count; i++)
+            {
+                if(CountriesList[i].Country_text == SelectCountry.Text)
+                {
+                    country = CountriesList[i].Country_text;
+                    date = CountriesList[i].LastUpdate;
+                    deaths = CountriesList[i].TotalDeathsText;
+                    cases = CountriesList[i].TotalCasesText;
+                }
+            }
+
+            CountryName.Text = country;
+            Date.Text = date.Substring(0,10);
+            ConfD.Text = deaths;
+            ConfC.Text = cases;
+
         }
     }
 }
